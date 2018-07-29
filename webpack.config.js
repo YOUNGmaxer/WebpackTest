5ea1f8abf2b33dc16376d8c9ff07f4e0e455dac0
YOUNGmaxer <517538470@qq.com>
2018-07-29 23:00:18 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: 'babel-loader',
+      },
+    ],
+  },
+
+  // 代码模块路径解析的配置
+  resolve: {
+
+    modules: [
+      "node_modules"
+    ]
+  }
+}
